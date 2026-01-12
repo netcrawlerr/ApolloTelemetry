b1fa1ed690a712bd2472301c2942f3a5f4a8acac
netcrawlerr <abdulmejidamk@gmail.com>
2026-01-12 20:16:04 +0300

    chore: project initialization

---
+++ src/ApolloTelemetry.Dashboard/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ApolloTelemetry.Dashboard.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ src/ApolloTelemetry.Dashboard/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ApolloTelemetry.Dashboard.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
